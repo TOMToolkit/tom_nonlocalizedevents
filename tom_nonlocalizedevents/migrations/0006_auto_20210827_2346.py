6f551a61eaa89129c3e4db0b867ea79c075aae81
Austin Riba <austin@m51.io>
2025-09-10 11:48:06 -0700

    Fix issue preventing migrations from running with modern tom toolkit

--- tom_nonlocalizedevents/migrations/0006_auto_20210827_2346.py
+++ tom_nonlocalizedevents/migrations/0006_auto_20210827_2346.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('superevent', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='tom_nonlocalizedevents.superevent')),
-                ('target', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='tom_targets.target')),
+                ('target', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='tom_targets.BaseTarget')),
             ],
         ),
         migrations.AddConstraint(
